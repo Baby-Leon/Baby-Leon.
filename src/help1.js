07f0e3b8f062c4cf84479669ce48dff3457ac7f7
Baby-Leon <leoriddle1@gmail.com>
2021-04-23 19:10:41 -0300

    First commit

---
+++ src/help1.js
@@ -0,0 +1,47 @@
+const help1 = (prefix) => {
+
+	return `
+ⵈ━══════╗⚚╔══════━ⵈ
+
+ *COMANDOS DO BABY LEON*
+ 
+   • 🄱🄰🄱🅈 🄻🄴🄾🄽 •
+ⵈ━══════╗⚚╔══════━ⵈ
+ 
+➸ *${prefix}marcar*
+➸ *${prefix}marcar2*
+➸ *${prefix}marcar3*
+➸ *${prefix}loli*
+➸ *${prefix}loli1*
+➸ *${prefix}heproibido*
+➸ *${prefix}dono*
+➸ *${prefix}pproibido*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}akeno*
+➸ *${prefix}meme*
+➸ *${prefix}lofi*
+➸ *${prefix}malkova*
+➸ *${prefix}canal*
+➸ *${prefix}nsfwloli*
+➸ *${prefix}reislin*
+➸ *${prefix}limpar*
+➸ *${prefix}marcar*
+➸ *${prefix}ts (texto que deseja transmitir)*
+
+════════════════════
+*Baby Leon* 👶🏼
+*Digite ${prefix}dono para mais info*
+════════════════════`
+
+}
+exports.help1 = help1
+
---
+++ src/zeusmenu.js
@@ -0,0 +1,40 @@
+const zeusmenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos do Zeus:*
+
+➸ *${prefix}loli*
+➸ *${prefix}heproibidopq*
+➸ *${prefix}dono*
+➸ *${prefix}pproibido*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+  FEITO POR *LEANDRO.*
+  DUVIDAS? 👇
+  WA.me/5527996467489
+╚════════════════════`
+}
+
+exports.leonmenu = leonmenu
+
+
+
+
+
+
+
+
